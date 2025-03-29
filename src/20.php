fa56f5852ab5ddf3724cd4ed4afb1b4068828538
bembowantonio416 <bembowantonio416@gmail.com>
2025-03-29 19:29:39 +0000

    Add 20.php

---
+++ src/20.php
@@ -0,0 +1,13 @@
+<?php
+$students = array(
+    '1' => "Alice",
+    '2' => "Bob",
+    '3' => "Charlie",
+    '4' => "David"
+);
+
+echo "The students are: ";
+foreach ($students as $student) {
+    echo "$student, ";
+}
+?>
