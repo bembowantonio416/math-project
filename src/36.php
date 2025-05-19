692cdfa6ff9b0627657eed02fe790b33cd132938
bembowantonio416 <bembowantonio416@gmail.com>
2025-05-19 02:45:53 +0000

    Add 36.php

---
+++ src/36.php
@@ -0,0 +1,20 @@
+<?php
+// Example PHP code for mathematical calculations
+$number1 = 5;
+$number2 = 3.0;
+
+// Addition
+echo "The sum of $number1 and $number2 is: " . ($number1 + $number2);
+
+// Subtraction
+echo "The difference between $number1 and $number2 is: " . ($number1 - $number2);
+
+// Multiplication
+echo "The product of $number1 and $number2 is: " . ($number1 * $number2);
+
+// Division
+echo "The quotient of $number1 divided by $number2 is: " . ($number1 / $number2);
+
+// Exponentiation
+echo "The result of $number1 raised to the power of $number2 is: " . pow($number1, $number2);
+?>
